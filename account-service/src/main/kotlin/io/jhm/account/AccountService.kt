f7f9f0b5686ec2888d44d5e647bc3f0fbfe73076
jhm <jhm@mz.co.kr>
2020-02-14 10:41:52 +0900

    jhm 변경

--- account-service/src/main/kotlin/io/jhm/account/AccountService.kt
+++ account-service/src/main/kotlin/io/jhm/account/AccountService.kt
@@ -1,8 +1,12 @@
 package io.jhm.account
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,4 +14,18 @@ class AccountService
 
 fun main(args: Array<String>) {
 	runApplication<AccountService>(*args)
+}
+
+@RefreshScope
+@RestController
+class TestController(
+	@Value("\${say.cheese}")private val say: String
+) {
+
+
+	@GetMapping("/say")
+	fun say(): String {
+		return say
+	}
+
 }
