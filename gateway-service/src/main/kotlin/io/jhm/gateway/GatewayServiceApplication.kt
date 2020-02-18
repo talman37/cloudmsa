87f39896064d83676b803c94cb859af4927f74d5
jhm <jhm@mz.co.kr>
2020-02-18 17:57:00 +0900

    zuul config 추가

--- gateway-service/src/main/kotlin/io/jhm/gateway/GatewayServiceApplication.kt
+++ gateway-service/src/main/kotlin/io/jhm/gateway/GatewayServiceApplication.kt
@@ -3,9 +3,11 @@ package io.jhm.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
+@SpringBootApplication
 class GatewayServiceApplication
 
 fun main(args: Array<String>) {
