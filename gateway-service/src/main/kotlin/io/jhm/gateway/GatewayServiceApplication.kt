e93c9bb1c10dc06efc5cbe417b20fbe6febaa1cf
jhm <jhm@mz.co.kr>
2020-02-18 17:48:27 +0900

    gatewayservice application 추가 gateway config 파일 추가

---
+++ gateway-service/src/main/kotlin/io/jhm/gateway/GatewayServiceApplication.kt
@@ -0,0 +1,13 @@
+package io.jhm.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayServiceApplication>(*args)
+}
